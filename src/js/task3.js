29e1da7785c18abb5f80a5e80dd40cc0a0b9617b
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-02-16 21:06:11 +0300

    task3

--- src/js/task3.js
+++ src/js/task3.js
@@ -1,5 +1,5 @@
 const input = document.getElementById("name-input");
 const output = document.getElementById("name-output");
 input.addEventListener("input", (event) => {
-  output.textContent = event.target.value;
+  output.textContent = event.target.value.trim();
 });
