e5c933fe84a8502fa20f12006b99d61451ba1fb3
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-02-16 14:13:39 +0300

    task 1 is done

--- src/js/task1.js
+++ src/js/task1.js
@@ -1,2 +1,7 @@
 const categories = document.querySelectorAll(".item-item");
 console.log(`number of categories: ${categories.length}`);
+categories.forEach((category) => {
+  const list = category.nextElementSibling;
+  const itemCount = list.children.length;
+  console.log(`Categories: ${category.textContent}\nElements: ${itemCount}`);
+});
