be72caebb83470f2d0c513c73edc7893e60bba69
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-02-16 18:07:44 +0300

    task3 is done

---
+++ src/js/task3.js
@@ -0,0 +1,5 @@
+const input = document.getElementById("name-input");
+const output = document.getElementById("name-output");
+input.addEventListener("input", (event) => {
+  output.textContent = event.target.value;
+});
