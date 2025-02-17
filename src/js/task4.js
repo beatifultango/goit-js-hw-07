0ae87dd02d892845ca29627659df86a581ed4ff3
Merve Delibaşoglu <mervedelibasoglu.dev@gmail.com>
2025-02-17 15:54:35 +0300

    task4 is done

--- src/js/task4.js
+++ src/js/task4.js
@@ -1,4 +1,23 @@
 const form = document.querySelector("form");
-form.addEventListener("submit", (event)=>{
-    event.preventDefault();
+const inputs = document.querySelectorAll("input");
+let isValid = true;
+let formDatas = {};
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  inputs.forEach((input) => {
+    if (input.value.trim() === "") {
+      isValid = false;
+    } else {
+      isValid = true;
+      formDatas[input.name] = input.value.trim();
+    }
+    if (!isValid) {
+      alert(`All areas must be fiiled in.`);
+      return;
+    }
+    
+  });
+  console.log(formDatas);
+  alert(`Form successfully submitted!`);
+  form.reset();
 });
